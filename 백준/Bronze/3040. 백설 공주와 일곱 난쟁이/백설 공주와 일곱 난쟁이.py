c6700bb527be9d7f4685cda103c718d4f04452d1
june <74134181+2cheongjun@users.noreply.github.com>
2022-06-29 00:41:58 +0900

    Update 백설 공주와 일곱 난쟁이.py

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.py
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.py
@@ -1,3 +1,4 @@
+* 부르트포스 풀이 
 arr = [int(input()) for _ in range(9)]
 
 totalSum = sum(arr)
@@ -15,3 +16,14 @@ for i in range(9):
         continue
     break
 print(*arr, sep="\n")
+
+
+
+* 조합풀이
+from itertools import combinations
+
+list = [int(input()) for i in range(9)]
+for i in combinations(list,7):
+    if sum(i)==100:
+        print(*i, sep='\n')
+        break
