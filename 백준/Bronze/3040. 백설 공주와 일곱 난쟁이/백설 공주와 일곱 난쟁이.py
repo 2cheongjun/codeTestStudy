435f1d1d70b9c2ec7fdabebd0d29c375c1ac6fce
june <74134181+2cheongjun@users.noreply.github.com>
2022-06-29 00:37:32 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.py
@@ -0,0 +1,17 @@
+arr = [int(input()) for _ in range(9)]
+
+totalSum = sum(arr)
+for i in range(9):
+    totalSum -= arr[i]
+    for j in range(i+1, 9):
+        totalSum -= arr[j]
+        if totalSum == 100:
+            arr.remove(arr[j])
+            arr.remove(arr[i])
+            break
+        totalSum += arr[j]
+    else:
+        totalSum += arr[i]
+        continue
+    break
+print(*arr, sep="\n")
