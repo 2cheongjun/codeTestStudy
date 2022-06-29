fc8f0a7c191e89f559d7bd42fef33ce9ba091aa9
june <74134181+2cheongjun@users.noreply.github.com>
2022-06-29 18:30:21 +0900

    [Bronze V] Title: 사칙연산, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+let input = readLine()!
+let arr = input.split(separator:" ")
+let A = Int(arr[0])!
+let B = Int(arr[1])!
+print(A+B)
+print(A-B)
+print(A*B)
+print(A/B)
+print(A%B)
