7ca3aabfa0543309d09504a19a2be7c3e6939600
june <74134181+2cheongjun@users.noreply.github.com>
2022-07-02 01:07:48 +0900

    Update 숫자의 개수.py

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -6,3 +6,14 @@ result = list(str(a*b*c))
 
 for i in range(10):
 	print(result.count(str(i)))
+
+
+	
+*방법2
+
+numbers = [int(input()) for _ in range(3)]
+
+result = list(str(numbers[0]* numbers[1]* numbers[2]))
+
+for i in range(10):
+	print(result.count(str(i)))
