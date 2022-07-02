4f688ac250ccd9c408e8621b462810ec5527befb
june <74134181+2cheongjun@users.noreply.github.com>
2022-07-02 18:41:16 +0900

    Update 알파벳 찾기.py

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -6,3 +6,19 @@ for i in c:
         print(s.index(i), end = ' ')
     else:
         print(-1, end=' ')
+        
+
+// 단어를 입력받아서 list에 담는다.        
+s = list(input())
+// 비교할 알파벳
+c = 'abcdefghijklmnopqrstuvwxyz'
+
+// a부터 z까지 S에 그 알바벳이 있는지 검사한다.
+for i in c:
+    // 단어에 i가 있으면 인덱스를 출력
+    if i in s:
+        print(s.index(i), end = ' ')
+    else:
+        // 없으면  -1을 출력
+        print(-1, end=' ')
+        
