a8f3bb6e0739a67d435a8f0ac605bd91c4800b6a
june <74134181+2cheongjun@users.noreply.github.com>
2022-07-03 23:02:04 +0900

    Update 상수.py

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,4 +1,10 @@
+*인덱스 활용
 a, b = (input().split())
 newa = int(a[2]+ a[1]+ a[0])
 newb = int(b[2]+ b[1] +b[0])
 print(max(newa, newb))
+
+
+*reversed()활용
+a, b = input().split()
+print(max("".join(reversed(a)), "".join(reversed(b))))
