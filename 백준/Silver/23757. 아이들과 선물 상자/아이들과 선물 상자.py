bbad596cf5e19c0b8feab4f1f5ac4d912eb23c40
june <74134181+2cheongjun@users.noreply.github.com>
2022-07-06 16:13:07 +0900

    Update 아이들과 선물 상자.py

--- 백준/Silver/23757. 아이들과 선물 상자/아이들과 선물 상자.py
+++ 백준/Silver/23757. 아이들과 선물 상자/아이들과 선물 상자.py
@@ -1,27 +1,3 @@
-import heapq as hq
-
-n, m = map(int, input().split())
-present = []
-for x in list(map(int, input().split())):
-    hq.heappush(present, -x) 
-wish = list(map(int, input().split()))
-
-ispre = False
-for i in wish:
-    x = -hq.heappop(present)
-    if x - i < 0:
-        ispre = True
-        break
-    hq.heappush(present, -(x-i))
-
-if ispre:
-    print(0)
-else:
-    print(1)
-    
-    
-    
-*****
 import heapq
 
 N, M = map(int, input().split())
@@ -50,3 +26,28 @@ for i in range(M):
     heapq.heappush(heap, -remain)
 
 print(1)
+
+
+*방법2
+import heapq as hq
+
+n, m = map(int, input().split())
+present = []
+for x in list(map(int, input().split())):
+    hq.heappush(present, -x) 
+wish = list(map(int, input().split()))
+
+ispre = False
+for i in wish:
+    x = -hq.heappop(present)
+    if x - i < 0:
+        ispre = True
+        break
+    hq.heappush(present, -(x-i))
+
+if ispre:
+    print(0)
+else:
+    print(1)
+    
+ 
