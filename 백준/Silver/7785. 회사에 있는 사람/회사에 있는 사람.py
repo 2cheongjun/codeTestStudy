42d7d54404eb5bcb14646bf64269bd7e5fbc1b76
june <74134181+2cheongjun@users.noreply.github.com>
2022-07-06 18:22:36 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 260 ms, Memory: 49028 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+nameIo = {}
+
+for x in range(n):
+    name, inout = map(str, sys.stdin.readline().split())
+    nameIo[name] = inout
+    if nameIo[name] == 'leave':
+       del nameIo[name]
+nameIo = sorted(nameIo, reverse=True)
+for k in nameIo:
+    print(k)
