f5c1458ee0470508bf9a7eb2f0973ef8f4ddc582
june <74134181+2cheongjun@users.noreply.github.com>
2022-07-07 18:05:11 +0900

    [Silver IV] Title: 좋은 단어, Time: 152 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+res = 0
+
+for _ in range(N):
+    temp = input().rstrip()
+    stack = []
+
+    for i in range(len(temp)):
+        if stack and temp[i] == stack[-1]:
+            stack.pop()
+
+        else:
+            stack.append(temp[i])
+
+    if not stack:
+        res += 1
+print(res)
