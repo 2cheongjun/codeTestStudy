dbef337da6b7ee2a1124bdf3220635deaf0001cb
june <74134181+2cheongjun@users.noreply.github.com>
2022-07-25 14:42:39 +0900

    Update A×B.swift

--- 백준/Bronze/10998. A×B/A×B.swift
+++ 백준/Bronze/10998. A×B/A×B.swift
@@ -6,3 +6,21 @@ let A = Int(arr[0])!
 let B = Int(arr[1])!
 
 print(A*B)
+
+
+
+파이썬
+A,B = input().split()
+print(int(A)*int(B))
+
+
+map사용
+A, B = map(int, input().split())
+print(A*B)
+
+
+N = (input().split())
+a = N[0]
+b = N[1]
+print(int(a)*int(b))
+
