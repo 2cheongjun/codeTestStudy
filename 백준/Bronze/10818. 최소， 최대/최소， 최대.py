6ed728d07669d987a59640581a9f18ea23b00a8e
june <74134181+2cheongjun@users.noreply.github.com>
2022-07-29 13:04:45 +0900

    Update 최소， 최대.py

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,3 +1,4 @@
+*min, max로 풀기 
 N = int(input())
 numbers = list(map(int, input().split()))
 print(min(numbers), max(numbers))
@@ -8,3 +9,17 @@ N=int(input())
 array=list(map(int,input().split()))
 array.sort()
 print(array[0],array[-1])#array[0], array[-1] index 처음과 마지막을 출력한다.
+
+
+*list index로 풀기
+n = int(input())
+numbers = list(map(int, input().split()))
+max  = numbers[0]
+min = numbers[0]
+
+for i in numbers[1:]:
+    if i > max:
+        max = i
+    elif i < min:
+        min = i
+print(min, max)
