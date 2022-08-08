09cbd24c00467548e09e2966bd24c157a234980a
june <74134181+2cheongjun@users.noreply.github.com>
2022-08-08 12:17:41 +0900

    [Bronze II] Title: OX퀴즈, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -1,13 +1,13 @@
-a = int(input())
-for i in range(a):
-    b = input()
-    s = list(b)
-    sum = 0
-    c = 1
-    for i in s:
-        if i == 'O':
-            sum += c
-            c += 1
+n = int(input()) #테스트케이스
+
+for i in range(n): #테스트케이스만큼 반복
+    ox = list(input()) #ox문자열을 받아 리스트에 넣기
+    sum = 0 #합
+    cnt = 0 #누적카운트
+    for x in ox:
+        if x == 'O':
+            cnt += 1
+            sum += cnt
         else:
-            c = 1
+            cnt = 0
     print(sum)
