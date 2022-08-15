390e86fd0d7752544b2005bde376c42b250ac0dd
june <74134181+2cheongjun@users.noreply.github.com>
2022-08-15 11:16:09 +0900

    [Bronze I] Title: 단어 공부, Time: 108 ms, Memory: 32796 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,12 +1,12 @@
-word = input().upper() #대문자
-only_word = list(set(word))#대표글자
+word = input().upper()
+only_word = list(set(word))
+cnt_list=[]
 
-cnt_list =[] #대표글자갯수
 for x in only_word:
     cnt = word.count(x)
     cnt_list.append(cnt)
 
-if cnt_list.count(max(cnt_list)) > 1 :
+if cnt_list.count(max(cnt_list)) > 1:
     print("?")
 else:
     max_index = cnt_list.index(max(cnt_list))
